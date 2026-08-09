6fd1b1c9c2bf73750b875cfadd0bb522bcdec6a2
rodrigocaracuelmolinillo-art <rodrigocaracuelmolinillo@gmail.com>
2026-08-09 11:42:22 +0000

    Clase Tablero

--- Tetris/tablero.py
+++ Tetris/tablero.py
@@ -1,1 +1,14 @@
-def game():
+import pygame
+
+class Tablero:
+    tablero = []
+    for i in range(20):
+        for j in range(10):
+            tablero[i][j] = "black"
+    def update ()
+
+    def draw(screen):
+        for i in range(20):
+            for j in range(10):
+                pygame.draw.rect(screen,tablero[i][j], 40*i, 40*j, 40, 40)
+
