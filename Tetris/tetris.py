17136456e5d80972f61ee0e7e75f63b12b3bb444
rodrigocaracuelmolinillo-art <rodrigocaracuelmolinillo@gmail.com>
2026-08-09 16:46:08 +0000

    que bonito

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,21 +1,22 @@
-import pygame,sys,Tablero, piezas
+import pygame
+import sys
+import Tablero
 from piezas import Piezas
 
+
 pygame.init()
+
+
 screen = pygame.display.set_mode((400,800))
 clock = pygame.time.Clock()
-running = True
-
 fcount = 0
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+nueva_pieza = Piezas()
 
+running = True
+while running:
     tablero = Tablero.init()
     Tablero.draw(tablero, screen)
 
-    nueva_pieza = Piezas()
     
     if nueva_pieza.cai_posible(tablero):
         tablero = caída(tablero)
@@ -23,20 +24,17 @@ while running:
         nueva_pieza = crear_pieza()
         nueva_pieza = draw(tablero)
     
-
-
-    if fcount % 60 == 0:
-        caida pieza
-
     
-
-    
-
-
-
-
-
     fcount += 1
+    if fcount % 30 == 0:
+        caida pieza
+    
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
     clock.tick(60)
+
 pygame.quit()
 sys.exit()
