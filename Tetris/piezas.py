a5b2e79b0f6931b6ecce4256f3de5454b414b06c
AlejandroRodriMilan <alejandrosidehustle@gmail.com>
2026-08-09 16:39:19 +0200

    cosas

--- Tetris/piezas.py
+++ Tetris/piezas.py
@@ -21,7 +21,7 @@ FORMAS = {
         [1, 1],
         [1, 0],
         [0, 0]
-    ]
+    ],
 
     "L": [
         [1, 0],
@@ -65,27 +65,35 @@ COLORES = {
 class Piezas:
     def __init__(self):
         self.tipo = crear_pieza
-        self.forma = FORMA[tipo]
+        self.forma = FORMAS[tipo]
         self.color = COLORES[tipo]
+        self.pos = [4,0]
 
     def crear_pieza(self):
-        tipo = random_choice(list.(FORMAS.keys()))
+        tipo = random_choice(list(FORMAS.keys()))
         return Pieza(tipo)
 
     def draw(self,tablero):
         for i in range(4):
-            for j in range(10):
+            for j in range(4,6):
                 if j==4 or j==5:
                     if FORMAS[self.tipo][i][j-4] == 1:
                         talero[i][j] = COLORES[self.tipo]
 
     def caída(self, tablero, pos):
+        ...
+
+    def rot_posible(self,tablero):
+        forma = self.rotar_derecha
+        for i in range(pos[0],pos[0]+len(forma)):
+            for j in range(len(forma[0])):
+
 
     def rotar_derecha(matriz):
-    return [
-        list(fila)
-        for fila in zip(*matriz[::-1])
-    ]
+        return [
+            list(fila)
+            for fila in zip(*matriz[::-1])
+        ]
 
 
 
