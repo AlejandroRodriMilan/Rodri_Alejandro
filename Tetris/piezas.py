511c5e50ce75ec98f1b13ae0b7c02c3d8a101454
rodrigocaracuelmolinillo-art <rodrigocaracuelmolinillo@gmail.com>
2026-08-09 14:09:06 +0000

    Piezass 2

--- Tetris/piezas.py
+++ Tetris/piezas.py
@@ -1,38 +1,54 @@
+import pygame, random, Tablero
+
+
 FORMAS = {
     "I": [
-        [1, 1, 1, 1]
+        [1, 0], 
+        [1, 0], 
+        [1, 0], 
+        [1, 0]
     ],
 
     "O": [
         [1, 1],
-        [1, 1]
+        [1, 1],
+        [0, 0],
+        [0, 0]
     ],
 
     "T": [
-        [0, 1, 0],
-        [1, 1, 1]
-    ],
+        [1, 0],
+        [1, 1],
+        [1, 0],
+        [0, 0]
+    ]
 
     "L": [
         [1, 0],
         [1, 0],
-        [1, 1]
+        [1, 1],
+        [0, 0]
     ],
 
     "J": [
         [0, 1],
         [0, 1],
-        [1, 1]
+        [1, 1],
+        [0, 0]
     ],
 
     "S": [
-        [0, 1, 1],
-        [1, 1, 0]
+        [1, 0],
+        [1, 1],
+        [0, 1],
+        [0, 0]
     ],
 
     "Z": [
-        [1, 1, 0],
-        [0, 1, 1]
+        [0, 1],
+        [1, 1],
+        [1, 0],
+        [0, 0]
     ]
 }
 
@@ -46,12 +62,37 @@ COLORES = {
     "Z": "red"
 }
 
-class piezas:
-    def __init__(self, tipo):
-        self.tipo = tipo
-        self.formas = FORMAS[tipo]
+class Piezas:
+    def __init__(self):
+        self.tipo = crear_pieza
+        self.forma = FORMA[tipo]
         self.color = COLORES[tipo]
 
     def crear_pieza(self):
+        tipo = random_choice(list.(FORMAS.keys()))
+        return Pieza(tipo)
+
+    def draw(self,tablero):
+        for i in range(4):
+            for j in range(10):
+                if j==4 or j==5:
+                    if FORMAS[self.tipo][i][j-4] == 1:
+                        talero[i][j] = COLORES[self.tipo]
+
+    def caída(self, tablero, pos):
+
+    def rotar_derecha(matriz):
+    return [
+        list(fila)
+        for fila in zip(*matriz[::-1])
+    ]
+
+
+
+                        
+                    
+
+
+
 
 
