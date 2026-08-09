64903e09d1fda351f357fc8afa5afcff609b55fd
AlejandroRodriMilan <alejandrosidehustle@gmail.com>
2026-08-09 11:20:04 +0000

    tetris.py and start of game.py

---
+++ Tetris/game.py
@@ -0,0 +1,2 @@
+def game(screen, clock):
+    
---
+++ Tetris/tetris.py
@@ -0,0 +1,15 @@
+import pygame,sys,game
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    game(screen, clock)
+    clock.tick(60)
+pygame.quit()
+sys.exit()
