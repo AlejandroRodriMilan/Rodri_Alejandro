fdc67bd2b0a6b9d53dad01932240b2dcbdba1b48
AlejandroRodriMilan <alejandrosidehustle@gmail.com>
2026-08-09 12:17:23 +0000

    Tablero 4 (el derroque de la clase)

--- Tetris/tablero.py
+++ Tetris/tablero.py
@@ -1,21 +1,18 @@
 import pygame
 
-class Tablero:
-    def __init__ (self)
-    self.filas = 20
-    self.columnas = 10
-    
-    self.tablero = []
-    for fila in range(self.filas):
-        for columna in range(self.columnas):
-            self.tablero[fila][columna] = "black"
-    def update ()
+def tablero_init():
+    tablero = []
+    for fila in range(20):
+        for columna in range(10):
+            tablero[fila][columna] = "black"
+    return tablero
 
-    def draw(self, screen):
-        for fila in range(self.filas):
-            for columna in range(self.columnas):
-                x = columnas*40
-                y = fila*40
-                cuadrado = pygame.Rect(x, y, 40, 40)
-                pygame.draw.rect(screen,self.tablero[fila][columna], cuadrado)
+
+def draw(tablero, screen):
+    for fila in range(20):
+        for columna in range(10):
+            x = columnas*40
+            y = fila*40
+            cuadrado = pygame.Rect(x, y, 40, 40)
+            pygame.draw.rect(screen,tablero[fila][columna], cuadrado)
 
--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,17 +1,18 @@
-import pygame,sys,game
+import pygame,sys
+from tablero import Tablero
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 clock = pygame.time.Clock()
 running = True
+tablero = Tablero()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    tablero = tablero_init()
     tablero.draw(screen)
-        
-    game(screen, clock)
     clock.tick(60)
 pygame.quit()
 sys.exit()
