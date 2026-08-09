7512d035db410eb4a6a97604daf8d438e1e35153
rodrigocaracuelmolinillo-art <rodrigocaracuelmolinillo@gmail.com>
2026-08-09 16:38:44 +0000

    Tetris acctualización

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,10 +1,12 @@
-import pygame,sys,Tablero
+import pygame,sys,Tablero, piezas
+from piezas import Piezas
 
 pygame.init()
 screen = pygame.display.set_mode((400,800))
 clock = pygame.time.Clock()
 running = True
 
+fcount = 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -12,6 +14,29 @@ while running:
 
     tablero = Tablero.init()
     Tablero.draw(tablero, screen)
+
+    nueva_pieza = Piezas()
+    
+    if nueva_pieza.cai_posible(tablero):
+        tablero = caída(tablero)
+    else:
+        nueva_pieza = crear_pieza()
+        nueva_pieza = draw(tablero)
+    
+
+
+    if fcount % 60 == 0:
+        caida pieza
+
+    
+
+    
+
+
+
+
+
+    fcount += 1
     clock.tick(60)
 pygame.quit()
 sys.exit()
