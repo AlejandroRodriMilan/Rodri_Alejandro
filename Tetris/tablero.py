30312b665a403a420d1bd38ed4fb6a14d6e43dd9
AlejandroRodriMilan <alejandrosidehustle@gmail.com>
2026-08-09 12:29:56 +0000

    hola caracola

--- Tetris/tablero.py
+++ Tetris/tablero.py
@@ -13,7 +13,7 @@ def init():
 def draw(tablero, screen):
     for fila in range(20):
         for columna in range(10):
-            x = columnas*40
+            x = columna*40
             y = fila*40
             cuadrado = pygame.Rect(x, y, 40, 40)
             pygame.draw.rect(screen,tablero[fila][columna], cuadrado)
